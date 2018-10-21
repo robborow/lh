426d4b16279ff70b7cdf6767304d2953559bbe87
Robert Borowiec <robert.borowiec@schibsted.com>
2018-10-21 10:06:34 +0200

    changes

--- lh.sh
+++ lh.sh
@@ -3,5 +3,5 @@ set -e # Exit on error
 
 echo "Running lighthouse tests..."
 
-docker login --username $ARTIFACTORY_USER --password $ARTIFACTORY_PWD containers.schibsted.io
+echo "$ARTIFACTORY_PWD" | docker login --username $ARTIFACTORY_USER --password-stdin containers.schibsted.io
 docker run -it --net=host --rm --cap-add=SYS_ADMIN containers.schibsted.io/smp-distribution/lighthouse-ci http://www.google.se --output=json
