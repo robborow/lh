eea3d93832b6dab81f4fae5bc35d9352468765a1
Robert Borowiec <robert.borowiec@schibsted.com>
2018-10-21 10:31:16 +0200

    changes

--- lh.sh
+++ lh.sh
@@ -3,9 +3,15 @@ set -e # Exit on error
 
 echo "Running lighthouse tests..."
 
+echo "TRAVIS_PULL_REQUEST"
 echo "$TRAVIS_PULL_REQUEST"
+
+
+echo "TRAVIS_PULL_REQUEST_SHA"
 echo "$TRAVIS_PULL_REQUEST_SHA"
+
+echo "TRAVIS_PULL_REQUEST_SLUG"
 echo "$TRAVIS_PULL_REQUEST_SLUG"
 
 echo "$DOCKER_PWD" | docker login --username $DOCKER_USER --password-stdin
-docker run -it --net=host --rm --cap-add=SYS_ADMIN docker pull robborow/lighthouse-ci http://www.google.se --output=json
+docker run -it --net=host --rm --cap-add=SYS_ADMIN obborow/lighthouse-ci http://www.google.se --quite --output=json
