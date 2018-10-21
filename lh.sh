7388c553639a88e046f5e3f2115d4daa431967b3
Robert Borowiec <robert.borowiec@schibsted.com>
2018-10-21 10:31:34 +0200

    typo

--- lh.sh
+++ lh.sh
@@ -14,4 +14,4 @@ echo "TRAVIS_PULL_REQUEST_SLUG"
 echo "$TRAVIS_PULL_REQUEST_SLUG"
 
 echo "$DOCKER_PWD" | docker login --username $DOCKER_USER --password-stdin
-docker run -it --net=host --rm --cap-add=SYS_ADMIN obborow/lighthouse-ci http://www.google.se --quite --output=json
+docker run -it --net=host --rm --cap-add=SYS_ADMIN robborow/lighthouse-ci http://www.google.se --quite --output=json
