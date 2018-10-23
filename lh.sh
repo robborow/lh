407cdcf1e9d19f2221c1e6cbe6793768af0037c7
Robert Borowiec <robert.borowiec@schibsted.com>
2018-10-23 21:50:48 +0200

    env var args

--- lh.sh
+++ lh.sh
@@ -15,4 +15,4 @@ echo "$TRAVIS_PULL_REQUEST_SLUG"
 
 echo "$DOCKER_PWD" | docker login --username $DOCKER_USER --password-stdin
 docker run -it --rm --cap-add=SYS_ADMIN robborow/lighthouse-ci http://www.google.se --output=json --quiet > report.json
-lighthouse-audit performance=97 accessibility=95
+npm run lighthouse-audit performance=$LIGHTOUSE_PERFORMANCE accessibility=$LIGHTOUSE_ACCESSABILITY
